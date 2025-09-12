c50a9a1512178a08fe2631c28157042373f75fcf
M4XR0HDE <maro27999@gmail.com>
2025-09-12 11:43:16 +0200

    more structure

--- Real_server.py
+++ Real_server.py
@@ -4,12 +4,14 @@ import socket
 
 
 
+
 HOST = '0.0.0.0'
 PORT = 65432
 def main():
     print(f"Server listening on {HOST}:{PORT}")
     print("Type 'exit' and press Enter to stop the server.")
-  
+    
+      
 
 if __name__ == "__main__":
     main()
